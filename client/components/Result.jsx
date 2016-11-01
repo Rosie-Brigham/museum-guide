e7b2de0417b5a8dcaa0bb19231fb37446d7b47c6
Rosie-Brigham <rosiebrigham@gmail.com>
2016-11-01 22:02:19 +0000

    adds the results component

---
+++ client/components/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+export default React.createClass({
+  mixins: [PureRenderMixin],
+  render: function() {
+    return <div className="winner">
+      You result is {this.props.result}! ooh yeah
+    </div>;
+  }
+});
