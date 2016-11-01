19033220f0be3c93cdaadc7a6fb3dafae9e8981f
Rosie-Brigham <rosiebrigham@gmail.com>
2016-11-01 22:02:28 +0000

    adds router into index

--- client/index.js
+++ client/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router';
 
 import App from './components/App'
+import Generator from './components/generator'
+
+
+const routes = <Route component={App}>
+  <Route path="/" component={Generator} />
+</Route>;
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App/>,
+    <Router history={hashHistory}>{routes}</Router>,
     document.getElementById('app')
   )
 })
