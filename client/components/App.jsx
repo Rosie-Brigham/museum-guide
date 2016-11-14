4530b632d6c45fceecce1a3121da9c6f19dccc83
Rosie-Brigham <rosiebrigham@gmail.com>
2016-11-14 09:50:40 +0000

    playing with router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,8 +8,15 @@ import store from '../store.js';
 
 import Home from './home.jsx'
 import Info from './info.jsx'
-import Generator from './generator.jsx'
+import Main from './generator.jsx'
 
+const hasVoted = false;
+
+// export default React.createClass({
+//   render: function() {
+//     return React.cloneElement(this.props.children, {hasVoted: hasVoted, result: "result"});
+//   }
+// });
 
 var App = React.createClass({
   getInitialState: function()
@@ -24,7 +31,7 @@ var App = React.createClass({
       <Provider store={ store }>
         <section>
           <Home/>
-          <Generator hasVoted="false" result="result"/>
+          { React.cloneElement(this.props.children, {hasVoted: hasVoted, result: "result"}) }
           <Info/>
         </section>
       </Provider>
--- client/index.js
+++ client/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router';
 
 import App from './components/App'
+import List from './components/List'
 import Generator from './components/generator'
 
 
 const routes = <Route component={App}>
   <Route path="/" component={Generator} />
+  <Route path="/list" component={List} />
 </Route>;
 
 document.addEventListener('DOMContentLoaded', () => {
